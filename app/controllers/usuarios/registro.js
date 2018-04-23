d5c7ec1a11420ca4bfcf53ad35dd427c479d03ab
Daniel Cano <dacanoc@unal.edu.co>
2018-04-22 19:29:16 -0500

    Feat: Add some actions, css and edit template usuarios/registro.hbs
    Load actions to load default values en form, edit css to image and form, console.log tests

--- app/controllers/usuarios/registro.js
+++ app/controllers/usuarios/registro.js
@@ -1,4 +1,43 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+    actions: {
+        registrar() {
+            let { nombreUsuario, contraseña, confirmacionContraseña, nombre, fechaNacimiento,
+                genero, ocupacion, biografia, fotoDePerfil, generoPreferido, ubicacionPreferida} =
+                this.getProperties('nombreUsuario', 'contraseña', 'confirmacionContraseña', 'nombre',
+                    'fechaNacimiento', 'genero', 'ocupacion', 'biografia', 'fotoDePerfil', 
+                    'generoPreferido', 'ubicacionPreferida');
+
+            console.log(nombreUsuario);
+            console.log(contraseña);
+            console.log(confirmacionContraseña);
+            console.log(nombre);
+            console.log(fechaNacimiento);
+            console.log(genero);
+            console.log(ocupacion);
+            console.log(biografia);
+            console.log(fotoDePerfil);
+            console.log(generoPreferido);
+            console.log(ubicacionPreferida);
+        },
+        seleccionarFoto: function (event) {
+            const reader = new FileReader();
+            const file = event.target.files[0];
+            let imageData;
+
+            reader.onload = () => {
+                imageData = reader.result;
+                this.set('fotoDePerfil', imageData);
+
+            };
+
+            if (file) {
+                reader.readAsDataURL(file);
+            }
+        },
+        seleccionarUbicacion (ubicacion) {
+            this.set('ubicacionPreferida', ubicacion);
+        }
+    }
 });
--- app/routes/usuarios/registro.js
+++ app/routes/usuarios/registro.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    setupController() {
+        this.controller.set('genero', 'femenino');
+        this.controller.set('generoPreferido', 'masculino');
+        this.controller.set('fotoDePerfil', '/default.png');
+        this.controller.set('ubicacionPreferida', 'ciudad');
+      },
 });
