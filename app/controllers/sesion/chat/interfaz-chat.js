00088a37711d730d632f74e4caf7f5fc21881480
hernan232 <hdvanegasm@gmail.com>
2018-05-19 10:53:44 -0500

    Fix: error de chat ( - )

--- app/controllers/sesion/chat/interfaz-chat.js
+++ app/controllers/sesion/chat/interfaz-chat.js
@@ -3,49 +3,52 @@ import { inject as service } from '@ember/service';
 import { filter } from 'rsvp';
 
 export default Controller.extend({
-    sesion: service('session'),
-    actions: {
-        enviar() {
-            let mensaje = this.get('contenidoMensaje');
+  sesion: service('session'),
+  actions: {
+    enviar() {
+      let mensaje = this.get('contenidoMensaje');
 
-            let nuevoMensaje = this.get('store').createRecord('mensaje', {
-                contenido: mensaje,
-                fecha: new Date()
-            });
+      let nuevoMensaje = this.get('store').createRecord('mensaje', {
+        contenido: mensaje,
+        fecha: new Date()
+      });
 
-            // Recupera el autor del Store
-            let autorMensaje = this.get('store').peekRecord('usuario', this.get('sesion').get('uid'));
-            nuevoMensaje.set('emisor', autorMensaje);
+      // Recupera el autor del Store
+      this.get('store').findRecord('usuario', this.get('sesion').get('uid')).then((autorMensaje) => {
+        nuevoMensaje.set('emisor', autorMensaje);
 
-            nuevoMensaje.set('chat', this.get('model'));
-            this.get('model').get('mensajes').pushObject(nuevoMensaje);
-            this.get('model').save().then(() => {
-                nuevoMensaje.save().then(() => {
-                    this.set('contenidoMensaje', '');
-                });
-            });
-        },
+        nuevoMensaje.set('chat', this.get('model'));
+        this.get('model').get('mensajes').pushObject(nuevoMensaje);
+        this.get('model').save().then(() => {
+          nuevoMensaje.save().then(() => {
+            this.set('contenidoMensaje', '');
+          });
+        });
+      });
+    },
 
-        cerrar() {
-            this.get('model').set('estado', 'finalizado')
-            this.get('model').save().then(() => {
-                this.transitionToRoute('sesion.usuario', this.get('sesion').get('uid'));
-            });
-        },
+    cerrar() {
+      this.get('model').set('estado', 'finalizado')
+      this.get('model').save().then(() => {
+        this.transitionToRoute('sesion.usuario', this.get('sesion').get('uid'));
+      });
+    },
 
-        agregar() {
-            let usuario = this.get('store').peekRecord('usuario', this.get('sesion').get('uid'));
-            this.get('model').get('favoritos').pushObject(usuario);
-            this.get('model').save().then((chat) => {
-                if(chat.get('favoritos').get('length') == 2) {
-                    let userArray = this.get('model').get('usuarios').filter(usuario => usuario.get('id') != this.get('session').get('uid'));
-                    console.log(usuario.get('listaFavoritos').get('usuarios'));
-                    //usuario.get('listaFavoritos').get('usuarios').pushObject(usuarioArray[0]);
-                    //usuario.save().then(() => {
+    agregar() {
+      this.get('store').findRecord('usuario', this.get('sesion').get('uid')).then( usuario => {
+        this.get('model').get('favoritos').pushObject(usuario);
+        this.get('model').save().then((chat) => {
+          if(chat.get('favoritos').get('length') == 2) {
 
-                    //})
-                }
-            })
-        }
+            //let userArray = this.get('model').get('usuarios').filter(usuario => usuario.get('id') != this.get('session').get('uid'));
+            //console.log(usuario.get('listaFavoritos').get('usuarios'));
+            //usuario.get('listaFavoritos').get('usuarios').pushObject(usuarioArray[0]);
+            //usuario.save().then(() => {
+
+            //})
+          }
+        });
+      });
     }
+  }
 });
