daa36ab5ebd2073ac9108d35be7465916a82f50e
hernan232 <hdvanegasm@gmail.com>
2018-05-20 13:56:22 -0500

    Documentation: documentacion de algunos modelos

--- app/models/solicitud-conexion.js
+++ app/models/solicitud-conexion.js
@@ -6,5 +6,10 @@ export default DS.Model.extend({
 
   emisor: DS.belongsTo('usuario', { inverse: null }),
   receptor: DS.belongsTo('usuario'),
+
+  // Como ocurre con el modelo chat, este es un atributo usado para optimizar
+  // el emparejamiento de chat cuando se hace una solicitud de conexion, este
+  // es un atributo puramente tecnico que no pertenece como tal al dominio y Por
+  // tanto no aparece ni el el diagrama de clases ni en el modelo del dominio
   chat: DS.belongsTo('chat')
 });
